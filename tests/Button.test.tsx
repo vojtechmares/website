969c103eb9e180189f2d7acf3b461658ef2bbac1
Vojtech Mares <iam@vojtechmares.com>
2022-12-09 02:13:27 +0100

    tests: add tests

---
+++ tests/Button.test.tsx
@@ -0,0 +1,19 @@
+import { expect, test } from 'vitest'
+import { render, screen, cleanup } from '@testing-library/react'
+import { Button } from '@/components/Button'
+
+test('button', () => {
+  render(<Button>Hello</Button>)
+
+  expect(screen.getByText('Hello')).toBeDefined()
+
+  cleanup()
+})
+
+test('button-with-link', () => {
+  render(<Button href="/country-road">Hello</Button>)
+
+  expect(screen.getByText('Hello').getAttribute('href')).toBe('/country-road')
+
+  cleanup()
+})
