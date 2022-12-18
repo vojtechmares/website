14a1667bd926045b376e38ef49149f46522802e7
Vojtech Mares <iam@vojtechmares.com>
2022-12-19 00:50:49 +0100

    chore(deps): remove dependency vite-tsconfig-paths
    - fix vitest.config.js and tsconfig path translation myself

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,19 @@
 
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths(),
   ],
   test: {
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '.')
+    }
   },
 })
